64402135327327743c95909b0eaf3c1142305e88
Asus <Asus@DESKTOP-E75SB5T>
2018-09-19 14:03:41 +0700

    Final

---
+++ src/BiodataKeluarga.java
@@ -0,0 +1,89 @@
+class BiodataKeluarga {
+
+     private String namaAyah, namaIbu, namaSaudara, namaKu, alamat, tempatlahir, tanggallahir, hobi, citacita, notelp;
+
+    public String getNamaAyah() {
+        return namaAyah;
+    }
+
+    public String getNamaIbu() {
+        return namaIbu;
+    }
+
+    public String getNamaSaudara() {
+        return namaSaudara;
+    }
+
+    public String getNamaKu() {
+        return namaKu;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public String getTempatlahir() {
+        return tempatlahir;
+    }
+
+    public String getTanggallahir() {
+        return tanggallahir;
+    }
+
+    public String getHobi() {
+        return hobi;
+    }
+
+    public String getCitacita() {
+        return citacita;
+    }
+
+    public String getNotelp() {
+        return notelp;
+    }
+
+    public void setNamaAyah(String namaAyah) {
+        this.namaAyah = namaAyah;
+    }
+
+    public void setNamaIbu(String namaIbu) {
+        this.namaIbu = namaIbu;
+    }
+
+    public void setNamaSaudara(String namaSaudara) {
+        this.namaSaudara = namaSaudara;
+    }
+
+    public void setNamaKu(String namaKu) {
+        this.namaKu = namaKu;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public void setTempatlahir(String tempatlahir) {
+        this.tempatlahir = tempatlahir;
+    }
+
+    public void setTanggallahir(String tanggallahir) {
+        this.tanggallahir = tanggallahir;
+    }
+
+    public void setHobi(String hobi) {
+        this.hobi = hobi;
+    }
+
+    public void setCitacita(String citacita) {
+        this.citacita = citacita;
+    }
+
+    public void setNotelp(String notelp) {
+        this.notelp = notelp;
+    }
+
+     
+}
+
+    
+
---
+++ src/Biodataku.java
@@ -0,0 +1,34 @@
+public class Biodataku {
+    
+    public static void main(String[] args){
+        BiodataKeluarga biodata = new BiodataKeluarga();
+        
+        biodata.setNamaAyah("Mochammad Yatim");
+        biodata.setNamaIbu("Ida Setijaningsih");
+        biodata.setNamaSaudara("Finka Fatihahsari");
+        biodata.setNamaKu("Faradila Rahmandasari");
+        biodata.setAlamat("Jl. Tangkilisan A-9 RT/RW 12 Lanud ABD SALEH, Pakis - Malang");
+        biodata.setTempatlahir("Malang");
+        biodata.setTanggallahir("19 Januari 2003");
+        biodata.setHobi("Menggambar");
+        biodata.setCitacita("Programmer");
+        biodata.setNotelp("081233881291");
+        
+        System.out.println("Nama Ayah       : " + biodata.getNamaAyah());
+        System.out.println("Nama Ibu        : " + biodata.getNamaIbu());
+        System.out.println("Nama Saudara    : " + biodata.getNamaSaudara());
+        System.out.println("Nama Saya       : " + biodata.getNamaKu());
+        System.out.println("Alamat          : " + biodata.getAlamat());
+        System.out.println("Tempat Lahir    : " + biodata.getTempatlahir());
+        System.out.println("Tanggal Lahir   : " + biodata.getTanggallahir());
+        System.out.println("Hobi            : " + biodata.getHobi());
+        System.out.println("Cita-cita       : " + biodata.getCitacita());
+        System.out.println("No. Telp        : " + biodata.getNotelp());
+            
+    }
+
+  
+
+    
+    }
+
